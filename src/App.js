f9be46bbb3fa947ead020fda5bb5f2def5465eb3
Gilberto Junior <junior.siilva182@gmail.com>
2021-06-14 11:21:42 -0300

    feat: added delete and toggle task functions

--- src/App.js
+++ src/App.js
@@ -24,10 +24,28 @@ function App() {
     },
   ]);
 
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks to Show'
+      )}
     </div>
   );
 }
