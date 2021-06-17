6df4a282a770653ac18583b4bacb02552ae56c0e
Gilberto Junior <junior.siilva182@gmail.com>
2021-06-17 17:40:58 -0300

    feat: added useLocation hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router';
 import Button from './Button';
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? 'red' : 'green'}
-        text={showAdd ? 'Close' : 'Add'}
-        onClick={onAdd}
-      />
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
